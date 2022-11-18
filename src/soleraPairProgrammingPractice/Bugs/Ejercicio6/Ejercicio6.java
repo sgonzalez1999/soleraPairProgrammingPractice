723f3d450704a7d9dfaa4e73cba2d20a19b1ef91
sgonzalez1999 <117899080+sgonzalez1999@users.noreply.github.com>
2022-11-18 16:19:23 +0100

    origin/ejercicio6 - se arregla el ejercicio6

--- src/soleraPairProgrammingPractice/Bugs/Ejercicio6/Ejercicio6.java
+++ src/soleraPairProgrammingPractice/Bugs/Ejercicio6/Ejercicio6.java
@@ -2,9 +2,11 @@ package soleraPairProgrammingPractice.Bugs.Ejercicio6;
 
 public class Ejercicio6 {
     private static byte char64( char x) {
-//      (int)x < 0 || the value of (int)x will always be positive
-        if ( (int)x > index_64.length)
-            return -1;
-        return index_64[(int) x] ;
-    }
+    	private static byte char64( char x) {
+//          (int)x < 0 || the value of (int)x will always be positive first bug
+//          changing the ‘>’ operator to ‘>=’: (int)x >= index_64.length. second bug
+            if ( (int)x >= index_64.length)
+                return -1;
+            return index_64[(int) x] ;
+        }
 }
