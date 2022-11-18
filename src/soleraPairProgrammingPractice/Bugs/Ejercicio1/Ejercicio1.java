69ce7f1fb549f5da18852797286eb58adb65c393
sgonzalez1999 <117899080+sgonzalez1999@users.noreply.github.com>
2022-11-18 13:05:04 +0100

    origin/ej1y2 - ej uno

--- src/soleraPairProgrammingPractice/Bugs/Ejercicio1/Ejercicio1.java
+++ src/soleraPairProgrammingPractice/Bugs/Ejercicio1/Ejercicio1.java
@@ -1,5 +1,15 @@
 package soleraPairProgrammingPractice.Bugs.Ejercicio1;
 
 public class Ejercicio1 {
+	
+	private boolean checkDimensions(CLIQUEUnit other , int e) {
+        for(int i=0, j=0; i<e; i++, j+=2) {
+            if(dims[i]!=other.dims[i] || bounds[j]!= other.bounds[j] || bounds[j-1] != other.bounds[j-1] ) {
+                return false;
+//              bounds[j-1] != bounds[j-1] change last condition
+            }
+        }
+        return true;
+    }
 
 }
