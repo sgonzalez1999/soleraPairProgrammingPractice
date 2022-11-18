6633b9efba86b4c17b479079869a720ee511ff2e
sgonzalez1999 <117899080+sgonzalez1999@users.noreply.github.com>
2022-11-18 16:28:04 +0100

    origin/ejercicio8 - se añade solucion ejercicio8

--- src/soleraPairProgrammingPractice/Bugs/Ejercicio8/Ejercicio8.java
+++ src/soleraPairProgrammingPractice/Bugs/Ejercicio8/Ejercicio8.java
@@ -2,4 +2,27 @@ package soleraPairProgrammingPractice.Bugs.Ejercicio8;
 
 public class Ejercicio8 {
 
+	private void shiftRightDestructive(int wordShifts,
+            int bitShiftsInWord,
+            boolean roundUp) 
+    {
+//      bitShiftsInWord should never be 0
+            if (wordShifts == 0 || bitShiftsInWord == 0) {
+                return;
+            }
+            assert (wordShifts >= 0);
+            assert (bitShiftsInWord >= 0);
+            assert (bitShiftsInWord < 32);
+            if (wordShifts >= 4) {
+                zeroClear();
+                return;
+            }
+            final int shiftRestore = 32 - bitShiftsInWord;
+            // check this because "123 << 32" will be 123.
+            final boolean noRestore = bitShiftsInWord == 0;
+            final int roundCarryNoRestoreMask = 1 << 31;
+            final int roundCarryMask = (1 << (bitShiftsInWord - 1));  // <=
+            ....
+    }
+	
 }
