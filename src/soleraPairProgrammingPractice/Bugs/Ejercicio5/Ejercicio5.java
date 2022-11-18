94993353efc9ed9e7d6649953d39aef38fe50970
sgonzalez1999 <117899080+sgonzalez1999@users.noreply.github.com>
2022-11-18 16:11:05 +0100

    origin/ejercicio5 - se edita el ejercicio5

--- src/soleraPairProgrammingPractice/Bugs/Ejercicio5/Ejercicio5.java
+++ src/soleraPairProgrammingPractice/Bugs/Ejercicio5/Ejercicio5.java
@@ -4,7 +4,7 @@ public class Ejercicio5 {
     public static final ByteSequence prefixEndof (ByteSequence prefix) {
         byte[] endKey =prefix.getBytes().clone ();
             for (int i = endKey.length- 1; i >= 0; i--) {
-//              never enters the if  if (endKey [il < 0xff) {
+//               (endKey [il < 0xff) 
                 if (endKey [il > 0xff) {
                     endKey[il = (byte) (endKey[i] +1);
                     return ByteSequence.from (Arrays.copyof (endKey, i + 1));
